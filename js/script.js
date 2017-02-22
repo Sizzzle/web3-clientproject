cf57c73b8fc0681a3b2d6d37ebe32ce7528be72a
Jason McKelvey <jason_mckelvey@outlook.com>
2017-02-22 16:34:08 -0600

    added beers section and fine tuned some styles for site/mobile

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 // JavaScript Document
 
+
+// Draw-in Animation
 var $svg = $('svg').drawsvg({
       duration: 5000,
       easing: 'linear'
     });
 
 $svg.drawsvg('animate');
+
+
+
+// Hidden Mobile Navigation
+$(document).ready(function(){
+	$('.mobile-nav-btn').click(function(e){
+		$('.mobile-nav').slideToggle();
+		// this prevents the browser from doing the default link action
+		e.preventDefault();
+	});
+	var $window = $(window);
+	  $window.on('resize', function (){
+        if ($window.width() > 800)
+        {
+            $('.mobile-nav').hide();
+        }
+    });
+});
+
